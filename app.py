fd944abda26e9b945131057d0c115755ab038466
bioroid17 <trace1997@ajou.ac.kr>
2024-09-06 05:52:47 +0900

    Now functioning
    Couldn't do streaming, though

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import openai.error
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import ChatPromptTemplate
@@ -7,13 +8,12 @@ from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.storage import LocalFileStore
 from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+import openai
 
 st.set_page_config(
     page_title="DocumentGPT",
     page_icon="📜",
 )
-
-llm = ChatOpenAI(temperature=0.1)
 
 
 @st.cache_data(show_spinner="Embedding file...")
@@ -31,7 +31,7 @@ def embed_file(file):
     )
     loader = UnstructuredFileLoader(file_path)
     docs = loader.load_and_split(text_splitter=splitter)
-    embeddings = OpenAIEmbeddings()
+    embeddings = OpenAIEmbeddings(api_key=API_KEY)
     cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
     vectorstore = FAISS.from_documents(docs, cached_embeddings)
     retriever = vectorstore.as_retriever()
@@ -56,6 +56,18 @@ def paint_history():
 
 def format_docs(docs):
     return "\n\n".join(document.page_content for document in docs)
+
+
+# OpenAI API key의 유효성을 검사합니다.
+# 반환 값은 파일 업로더의 비활성화 여부를 결정합니다.
+# disabled가 False일 때 버튼이 활성화되므로, API key가 유효하다면 False를 반환합니다.
+def validate_key(api_key):
+    try:
+        openai.api_key = api_key
+        openai.Model.list()
+        return False
+    except Exception:
+        return True
 
 
 prompt = ChatPromptTemplate.from_messages(
@@ -85,11 +97,161 @@ Upload your files on the sidebar.
 )
 
 with st.sidebar:
+    API_KEY = st.text_input("Enter your OpenAI API key", type="password")
+    is_invalid = validate_key(API_KEY)
     file = st.file_uploader(
-        "Upload a .txt .pdf or .docx file.", type=["pdf", "txt", "docx"]
+        "Upload a .txt .pdf or .docx file.",
+        type=["pdf", "txt", "docx"],
+        disabled=is_invalid,
     )
+    st.link_button(
+        "Github repo",
+        "https://github.com/bioroid17/nomad-gpt-study/tree/streamlit",
+    )
+    with st.expander("View source code"):
+        st.markdown(
+            """
+```python
+import openai.error
+import streamlit as st
+from langchain.chat_models import ChatOpenAI
+from langchain.prompts import ChatPromptTemplate
+from langchain.document_loaders import UnstructuredFileLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings, CacheBackedEmbeddings
+from langchain.vectorstores import FAISS
+from langchain.storage import LocalFileStore
+from langchain.schema.runnable import RunnablePassthrough, RunnableLambda
+import openai
+
+st.set_page_config(
+    page_title="DocumentGPT",
+    page_icon="📜",
+)
+
+
+@st.cache_data(show_spinner="Embedding file...")
+def embed_file(file):
+    file_content = file.read()
+    file_path = f"./.cache/files/{file.name}"
+    with open(file_path, "wb") as f:
+        f.write(file_content)
+
+    cache_dir = LocalFileStore(f"./.cache/embeddings/{file.name}")
+    splitter = CharacterTextSplitter.from_tiktoken_encoder(
+        separator="\n",
+        chunk_size=600,
+        chunk_overlap=100,
+    )
+    loader = UnstructuredFileLoader(file_path)
+    docs = loader.load_and_split(text_splitter=splitter)
+    embeddings = OpenAIEmbeddings(api_key=API_KEY)
+    cached_embeddings = CacheBackedEmbeddings.from_bytes_store(embeddings, cache_dir)
+    vectorstore = FAISS.from_documents(docs, cached_embeddings)
+    retriever = vectorstore.as_retriever()
+    return retriever
+
+
+def send_message(message, role, save=True):
+    with st.chat_message(role):
+        st.markdown(message)
+    if save:
+        st.session_state["messages"].append({"message": message, "role": role})
+
+
+def paint_history():
+    for message in st.session_state["messages"]:
+        send_message(
+            message["message"],
+            message["role"],
+            save=False,
+        )
+
+
+def format_docs(docs):
+    return "\n\n".join(document.page_content for document in docs)
+
+
+# OpenAI API key의 유효성을 검사합니다.
+# 반환 값은 파일 업로더의 비활성화 여부를 결정합니다.
+# disabled가 False일 때 버튼이 활성화되므로, API key가 유효하다면 False를 반환합니다.
+def validate_key(api_key):
+    try:
+        openai.api_key = api_key
+        openai.Model.list()
+        return False
+    except Exception:
+        return True
+
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            '''
+            Answer the question using ONLY the following context. If you don't know the answer just say you don't know. DON'T make anything up.
+            
+            Context: {context}
+            ''',
+        ),
+        ("human", "{question}"),
+    ]
+)
+
+st.title("DocumentGPT")
+
+st.markdown(
+    '''
+Welcome!
+            
+Use this chatbot to ask questions to an AI about your files!
+
+Upload your files on the sidebar.
+'''
+)
+
+with st.sidebar:
+    API_KEY = st.text_input("Enter your OpenAI API key", type="password")
+    is_invalid = validate_key(API_KEY)
+    file = st.file_uploader(
+        "Upload a .txt .pdf or .docx file.",
+        type=["pdf", "txt", "docx"],
+        disabled=is_invalid,
+    )
+    st.link_button(
+        "Github repo",
+        "https://github.com/bioroid17/nomad-gpt-study/tree/streamlit",
+    )
+    st.markdown('''
+        (코드 전문)
+        ''')
 
 if file:
+    llm = ChatOpenAI(temperature=0.1, api_key=API_KEY)
+    retriver = embed_file(file)
+    send_message("I'm ready! Ask away!", "ai", save=False)
+    paint_history()
+    message = st.chat_input("Ask anything about your file...")
+    if message:
+        send_message(message, "human")
+        chain = (
+            {
+                "context": retriver | RunnableLambda(format_docs),
+                "question": RunnablePassthrough(),
+            }
+            | prompt
+            | llm
+        )
+        response = chain.invoke(message)
+        send_message(response.content, "ai")
+else:
+    st.session_state["messages"] = []
+```
+"""
+        )
+
+if file:
+    llm = ChatOpenAI(temperature=0.1, api_key=API_KEY)
     retriver = embed_file(file)
     send_message("I'm ready! Ask away!", "ai", save=False)
     paint_history()
