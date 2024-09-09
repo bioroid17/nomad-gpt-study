b2aad0b710fdeefd5c14737aedcccf24acec0b56
bioroid17 <trace1997@ajou.ac.kr>
2024-09-09 21:53:24 +0900

    Difficulty setting done

--- app.py
+++ app.py
@@ -5,7 +5,9 @@ from langchain.retrievers import WikipediaRetriever
 from langchain.chat_models import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.callbacks import StreamingStdOutCallbackHandler
+from langchain.schema.runnable import RunnablePassthrough
 import streamlit as st
+import openai
 
 
 st.set_page_config(
@@ -26,6 +28,9 @@ function = {
     "parameters": {
         "type": "object",
         "properties": {
+            "difficulty": {
+                "type": "string",
+            },
             "questions": {
                 "type": "array",
                 "items": {
@@ -52,19 +57,22 @@ function = {
                     },
                     "required": ["question", "answers"],
                 },
-            }
+            },
         },
         "required": ["questions"],
     },
 }
 
+API_KEY = ""
+
 llm = ChatOpenAI(
     temperature=0.1,
-    model="gpt-3.5-turbo-0125",
+    model="gpt-4o-mini",
     streaming=True,
     callbacks=[
         StreamingStdOutCallbackHandler(),
     ],
+    api_key=API_KEY,
 ).bind(
     function_call={
         "name": "create_quiz",
@@ -83,6 +91,10 @@ prompt = PromptTemplate.from_template(
     Each question should have 4 answers, three of them must be incorrect and one should be correct.
          
     Use (o) to signal the correct answer.
+
+    Also, you will create the quiz depending on the difficulty that the user wants.
+
+    The difficulty will be either 'Easy' or 'Hard'.
          
     Question examples:
          
@@ -99,10 +111,24 @@ prompt = PromptTemplate.from_template(
     Answers: A Roman Emperor(o)|Painter|Actor|Model
          
     Your turn!
+
+    Difficulty: {difficulty}
          
     Context: {context}
 """
 )
+
+
+# OpenAI API key의 유효성을 검사합니다.
+# 반환 값은 파일 업로더의 비활성화 여부를 결정합니다.
+# disabled가 False일 때 버튼이 활성화되므로, API key가 유효하다면 False를 반환합니다.
+def validate_key(api_key):
+    try:
+        openai.api_key = api_key
+        openai.Model.list()
+        return False
+    except Exception:
+        return True
 
 
 @st.cache_data(show_spinner="Loading file...")
@@ -123,8 +149,15 @@ def split_file(file):
 
 
 @st.cache_data(show_spinner="Making quiz...")
-def run_quiz_chain(_docs, topic):
-    chain = {"context": format_docs} | prompt | llm
+def run_quiz_chain(_docs, topic, difficulty):
+    chain = (
+        {
+            "context": format_docs,
+            "difficulty": get_difficulty,
+        }
+        | prompt
+        | llm
+    )
     return chain.invoke(_docs)
 
 
@@ -135,7 +168,13 @@ def wiki_search(term):
     return docs
 
 
+def get_difficulty(_):
+    return difficulty
+
+
 with st.sidebar:
+    API_KEY = st.text_input("Enter your OpenAI API key", type="password")
+    is_invalid = validate_key(API_KEY)
     docs = None
     topic = None
     choice = st.selectbox(
@@ -144,19 +183,33 @@ with st.sidebar:
             "File",
             "Wikipedia Article",
         ),
+        disabled=is_invalid,
     )
     if choice == "File":
         file = st.file_uploader(
             "Upload a .docx, .txt or .pdf file",
             type=["pdf", "txt", "docx"],
+            disabled=is_invalid,
         )
         if file:
             docs = split_file(file)
     else:
-        topic = st.text_input("Name of the article")
+        topic = st.text_input(
+            "Name of the article",
+            disabled=is_invalid,
+        )
         if topic:
             docs = wiki_search(topic)
-    show_answer = st.toggle("Show answers when submitted", False)
+    difficulty = st.selectbox(
+        "Choose difficulty.",
+        ("Easy", "Hard"),
+        disabled=is_invalid,
+    )
+    show_answer = st.toggle(
+        "Show answers when submitted",
+        False,
+        disabled=is_invalid,
+    )
 
 if not docs:
     st.markdown(
@@ -169,7 +222,7 @@ if not docs:
     """
     )
 else:
-    response = run_quiz_chain(docs, topic if topic else file.name)
+    response = run_quiz_chain(docs, topic if topic else file.name, difficulty)
     data_str = response.additional_kwargs["function_call"]["arguments"]
     data = json.loads(data_str)
     questions = data["questions"]
